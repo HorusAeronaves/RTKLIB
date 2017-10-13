395006b3965dc41ab8c81137ec1da1e3aa26e959
Patrick José Pereira <patrickelectric@gmail.com>
2017-10-13 18:09:58 -0300

    window: Correct bug in windows with comma
    Signed-off-by: Patrick José Pereira <patrickelectric@gmail.com>

--- app/rtkuser-qt/src/window.cpp
+++ app/rtkuser-qt/src/window.cpp
@@ -220,9 +220,9 @@ void Window::runRTKLIB()
 {
     QString runningString = QStringLiteral("Running ... \t|\t%1 / %2");
 
-    QString baseAltInput = QString::number(ui->baseAltInput->text().split("+")[0].toFloat() + ui->AntAltInput->text().toFloat());
+    QString baseAltInput = QString::number(ui->baseAltInput->text().split("+")[0].replace(",", ".").toFloat() + ui->AntAltInput->text().replace(",", ".").toFloat());
     if(ui->checkBaseRtk->isChecked() && !ui->baseAltInput->text().contains("+")) {
-        ui->baseAltInput->setText(ui->baseAltInput->text() + " + " + ui->AntAltInput->text());
+        ui->baseAltInput->setText(ui->baseAltInput->text().replace(",", ".") + " + " + ui->AntAltInput->text().replace(",", "."));
     }
 
     uint step = 1;
